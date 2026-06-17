b3caea6f69b0cf55b52b48bca3e189fb9bb228a9
BananaKingXO <BananKingVr@gmail.com>
2026-06-16 19:11:16 -0500

    ✨ More work on 26_1 overlay

--- bundle.sh
+++ bundle.sh
@@ -9,7 +9,7 @@ rm -rf artifacts/${version}
 mkdir -p artifacts/${version}
 
 cd datapack/
-zip -r ../artifacts/${version}/Stellarity-${version}.zip data/ pack.mcmeta pack.png
+zip -r ../artifacts/${version}/Stellarity-${version}.zip data/ overlay_26_1/ overlay_1_21_11/ pack.mcmeta pack.png
 cp ../artifacts/${version}/Stellarity-${version}.zip ../artifacts/${version}/mod.zip
 cd ../music_pack
 zip -9 -r ../artifacts/${version}/Stellarity-${version}-Music.zip *
